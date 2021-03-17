9d40b59073ce5ab70a596d8d58c151c72724085c
Ahtem707 <asitzhalilov@gmail.com>
2021-03-17 22:01:18 +0300

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" temporary>
+      <v-list>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item
+            v-for="link of links"
+            :key="link.title"
+            :href="link.url">
+            <v-list-item-title>{{link.title}}</v-list-item-title>
+            <!-- <v-icon left>{{link.icon}}</v-icon> -->
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app dark color="blue">
+      <v-app-bar-nav-icon @click="drawer=!drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="link of links"
+          :key="link.title"
+          :href="link.url"
+          text>
+          <!-- <v-icon left>{{link.icon}}</v-icon> -->
+          {{link.title}}
+        </v-btn>
+        <v-btn text>Link Two</v-btn>
+        <v-btn text>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {title:'Login', icon: 'lock', url: '/login'},
+        {title:'Registration', 'face': 'lock', url: '/registration'},
+        {title:'Orders', icon: 'bookmark_border', url: '/orders'},
+        {title:'New ad', icon: 'node_add', url: '/new'},
+        {title:'My ads', icon: 'list', url: '/list'},
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <v-container>
+    <v-layout row>
+      <v-form>
+        <v-text-field
+          prepend-icon="person"
+          name="email"
+          label="Email"
+          type="email"
+          v-model="email"
+        ></v-text-field>
+        <v-text-field
+          id="password"
+          prepend-icon="lock"
+          name="password"
+          label="Password"
+          type="password"
+          v-model="password"
+        ></v-text-field>
+      </v-form>
+    </v-layout>
+  </v-container> -->
+
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <h1>Login</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
